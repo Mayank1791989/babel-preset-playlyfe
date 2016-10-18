107888bc2dce0a526fbc197c396fd4acfa60a9ed
Mayank Agarwal <mayank@playlyfe.com>
2016-10-18 16:25:49 +0530

    fix(build): fix missing package in dependency causing test to fail

--- src/plugins/__tests__/react-intl-test.js
+++ src/plugins/__tests__/react-intl-test.js
@@ -6,7 +6,11 @@ const fixtureFile = path.join(__dirname, './data', 'component.js');
 describe('check intl messages in transformed file metadata', () => {
   const transform = transformFileSync(fixtureFile, {
     babelrc: false,
-    presets: ['es2015', 'stage-0', 'react'],
+    presets: [
+      require('babel-preset-es2015'),
+      require('babel-preset-stage-0'),
+      require('babel-preset-react'),
+    ],
     plugins: [require('../react-intl').default],
   });
   it('exports 6 intl messages', () => {
